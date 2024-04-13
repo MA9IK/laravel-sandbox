769d82bd93933e9567eb56040e3a757c33c95329
Ivan Pen <developerjsua@gmail.com>
2024-04-13 19:31:03 +0300

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+Hello! It's my way i'm learning Laravel with online courses in youtube!
+This repo is just sandbox to play around with Laravel and all this fancy Laravel's stuff.
---
+++ resources/views/jobs.blade.php
@@ -0,0 +1,19 @@
+<x-layout>
+    <x-slot:heading>
+        Job Listing
+    </x-slot:heading>
+    <ul>
+
+        @foreach ($jobs as $job)
+        <li>
+            <a href="/jobs/{{ $job['id'] }}" class="hover:underline">
+                <strong>
+                    {{ $job['title'] }}:
+                </strong>
+                Pays {{ $job['salary'] }} per year.
+            </a>
+        </li>
+        @endforeach
+
+    </ul>
+</x-layout>
