e1f9864d4e5af55ccc311a654cc3ba6243909410
Ivan Pen <developerjsua@gmail.com>
2024-04-22 18:28:50 +0300

    csrf, filled vs. guarded fields, structure

--- routes/web.php
+++ routes/web.php
@@ -6,17 +6,32 @@ use App\Models\Job;
 Route::get('/', fn () => view('home'));
 
 Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->simplePaginate(3);
+    $jobs = Job::with('employer')->latest()->simplePaginate(3);
 
-    return view('jobs', [
+    return view('jobs.index', [
         'jobs' => $jobs
     ]);
+});
+
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
+});
+
+Route::post('/jobs', function () {
+    // validation...
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1,
+    ]);
+
+    return redirect('/jobs');
 });
 
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
 
-    return view('job', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
 });
 
 Route::get('/contact', fn () => view('contact'));
